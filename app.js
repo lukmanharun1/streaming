0614e91c2d8339d02efb2db76af9c46362a4b45a
lukman harun <lukman@skinmystery.com>
2022-09-07 13:05:22 +0700

    ubah asnycronous

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
@@ -15,27 +12,28 @@ app.use(express.urlencoded({ extended: false }));
 
 // app.use("/", indexRouter);
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const range = req.headers.range;
     const videoPath = "belajar.mp4";
-    // const videoPath = path.join(__dirname, "controllers/belajar.mp4");
-    const videoSize = fs.statSync(videoPath).size;
-    const chunkSize = 1_000_000; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + chunkSize, videoSize - 1);
+    fs.stat(videoPath, (err, dataVideo) => {
+      const { size: videoSize } = dataVideo;
+      const chunkSize = 1_000_000; // 1MB
+      const start = Number(range.replace(/\D/g, ""));
+      const end = Math.min(start + chunkSize, videoSize - 1);
 
-    const contentLength = end - start * 1;
+      const contentLength = end - start + 1;
 
-    const headers = {
-      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": contentLength,
-      "Content-Type": "video/mp4",
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+      const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(206, headers);
+      const videoStream = fs.createReadStream(videoPath, { start, end });
+      videoStream.pipe(res);
+    });
   } catch (error) {
     console.log(error);
     return res.status(error.statusCode || 500).json({
